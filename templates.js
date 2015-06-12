e5d5b499814bedf9c6baa783e0f5e7091947812b
Shaun P Sullivan <shaun@fisheyesoup.com>
2015-06-12 16:32:07 -0400

    added some text

--- templates.js
+++ templates.js
@@ -2,10 +2,9 @@ var templates = {};
 
 templates.newMessage = [
   "<ul class = 'messageContainer'>",
-<<<<<<< HEAD
+
   "<li class = 'messageContent' data-id = '<%= _id%>'><a href='#' class='fa fa-trash-o'></a><%= message %></li>",
-=======
->>>>>>> b642c221ac460270dc17f043677233e4070d6eda
+
   "<li class = 'messageCreator arrow_box'><%= username %></li>",
   "<li class = 'messageContent' data-id = '<%= _id%>'><%= message %></li>",
   "<li class = 'trash'><a href='#' class='fa fa-trash-o'></a></li>",
