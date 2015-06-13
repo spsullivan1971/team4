c7f0fb49d8bd6443a92f4304241140add78ec9d1
datLucius <jlnelsonart@gmail.com>
2015-06-13 18:23:19 -0400

    cleaned up design, more responsive

--- templates.js
+++ templates.js
@@ -4,15 +4,15 @@ templates.newMessage = [
   "<ul class = 'messageContainer' data-id = '<%= _id%>'>",
   "<li class = 'messageCreator arrow_box'><%= username %></li>",
   "<li class = 'messageContent'><%= message %></li>",
-  "<li class = 'trash'><a href='#' class='fa fa-trash-o'></a></li>",
+  "<li class = 'trash'><a href='#' class='fa fa-times'></a></li>",
   "</ul>"
 ].join("");
 
 templates.username = [
-  "<span>You are <span class='username'><%= username %></span></span>"
+  "<span class='youAreText'>...you are<span class='username'><%= username %></span></span>"
 ].join("");
 
 templates.usernameId = [
-  "<div class='changeUserName'><a href='#'>Change Space Name</a></div>",
-  "<span>You are <span class='username' data-id = '<%= _id%>'><%= username %></span></span>"
+  "<div class='changeUserName'><a href='#' class='fa fa-sign-out'></a></div>",
+  "<span class='youAreText'>...you are <span class='username' data-id = '<%= _id%>'><%= username %></span></span>"
 ].join("");
